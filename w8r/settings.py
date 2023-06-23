be46512d80bf5b7492638a6797869d861d5b7cb6
Ruslan Poltayev <poltayev@gmail.com>
2023-06-23 06:55:08 +0000

    - install needed libraries

--- w8r/settings.py
+++ w8r/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'rest_framework',
+    'django_filters',
     "debug_toolbar",
 
     'restaurant',
@@ -142,3 +143,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False,
+}
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
